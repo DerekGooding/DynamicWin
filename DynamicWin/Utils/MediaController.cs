56644db06e0e398012f5f65fb64c751f625243fb
Derek Gooding <djgooding@live.com>
2024-08-14 14:20:22 -0400

    issue with user32.dll import

--- DynamicWin/Utils/MediaController.cs
+++ DynamicWin/Utils/MediaController.cs
@@ -5,15 +5,15 @@ namespace DynamicWin.Utils;
 public partial class MediaController
 {
     [LibraryImport("user32.dll")]
-    private static partial void keyboard_event(byte bVk, byte bScan, uint dwFlags, int dwExtraInfo);
+    private static partial void keybd_event(byte bVk, byte bScan, uint dwFlags, int dwExtraInfo);
 
     private const byte VK_MEDIA_PLAY_PAUSE = 0xB3;
     private const byte VK_MEDIA_NEXT_TRACK = 0xB0;
     private const byte VK_MEDIA_PREV_TRACK = 0xB1;
 
-    public static void PlayPause() => keyboard_event(VK_MEDIA_PLAY_PAUSE, 0, 0, 0);
+    public static void PlayPause() => keybd_event(VK_MEDIA_PLAY_PAUSE, 0, 0, 0);
 
-    public static void Next() => keyboard_event(VK_MEDIA_NEXT_TRACK, 0, 0, 0);
+    public static void Next() => keybd_event(VK_MEDIA_NEXT_TRACK, 0, 0, 0);
 
-    public static void Previous() => keyboard_event(VK_MEDIA_PREV_TRACK, 0, 0, 0);
+    public static void Previous() => keybd_event(VK_MEDIA_PREV_TRACK, 0, 0, 0);
 }
